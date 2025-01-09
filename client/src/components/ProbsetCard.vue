a275443ccfcccbd41735efc1d5c2ab9c4a33c948
djdjz7 <djdjz7@icloud.com>
2025-01-09 13:05:41 +0800

    use consistent colors

--- client/src/components/ProbsetCard.vue
+++ client/src/components/ProbsetCard.vue
@@ -19,7 +19,7 @@ defineProps<{
   <div rounded-xl shadow-lg p-4 flex="~ col gap-2">
     <span text-2xl font-bold>{{ probset.probset.title }}</span>
     <span>{{ dateToString(probset.probset.openTime) }} - {{ dateToString(probset.probset.closeTime) }}</span>
-    <span self-end text-xl font-bold text-red>{{ probset.ac }}/{{ probset.total }} = {{ (probset.acRate *
+    <span self-end text-xl font-bold text-red-500>{{ probset.ac }}/{{ probset.total }} = {{ (probset.acRate *
       100).toFixed(2) }}%</span>
   </div>
 </template>
